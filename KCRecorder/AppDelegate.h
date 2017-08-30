bda4225ae467793257483532faaf0b4cef589bff
iMac <121700354@qq.com>
2017-08-30 09:58:23 +0800

    Initial Commit

---
+++ KCRecorder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KCRecorder
+//
+//  Created by iMac on 2017/8/30.
+//  Copyright © 2017年 iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KCRecorder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KCRecorder
+//
+//  Created by iMac on 2017/8/30.
+//  Copyright © 2017年 iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
